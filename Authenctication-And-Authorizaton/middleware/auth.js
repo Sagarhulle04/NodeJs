d009acd7534ecf2b796a0025283e074d61c6d761
Sagar Hulle <sagarhulle22@gmail.com>
2026-05-13 10:51:21 +0530

    corrected the status code in authentication and authorization folder

--- Authenctication-And-Authorizaton/middleware/auth.js
+++ Authenctication-And-Authorizaton/middleware/auth.js
@@ -8,7 +8,7 @@ export const loggedIn = async function (req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
 
     if (!token) {
-      res.status(200).json({ message: "Please enter a token" });
+      res.status(401).json({ message: "Please enter a token" });
     }
 
     const decoded = jwt.verify(token, process.env.secretToken);
